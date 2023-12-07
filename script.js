17c21dbb46f891e321d6a728f0bcb98c5b6438b3
ArthurZambiasi <137405874+ArthurZambiasi@users.noreply.github.com>
2023-12-07 08:41:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+document.getElementById('mostrarDiv').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivlieutenant');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('mostrarDiv2').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivLiterally');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('mostrarDiv3').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivWorst');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('mostrarDiv4').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivSquirrel');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('mostrarDiv5').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivBeach');
+
+    if (minhaDiv.style.display === 'none') {
+    minhaDiv.style.display = 'block';
+    } 
+    else {
+    minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('mostrarDiv6').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivDoughnut');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('mostrarDiv7').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivLightning');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('mostrarDiv8').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivJewerly');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('mostrarDiv9').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivTeeth');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('mostrarDiv10').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('minhaDivThief');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
